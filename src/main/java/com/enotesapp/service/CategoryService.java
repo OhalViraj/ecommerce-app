cae338d2fcb5e9a0de9e7daf0950bebab4db2b9f
Ohal Viraj <er.ohalviraj@gmail.com>
2026-03-24 16:55:42 +0530

    save get category implements

---
+++ src/main/java/com/enotesapp/service/CategoryService.java
@@ -0,0 +1,12 @@
+package com.enotesapp.service;
+
+import java.util.List;
+
+import com.enotesapp.entity.Category;
+
+public interface CategoryService {
+
+	public Boolean saveCategory(Category category);
+	
+	public List<Category> getAllCategory();
+}
